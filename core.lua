1b176e3a5f5a344e1c451b4a9c24567dc54c223d
unknown <spirtirs1337@gmail.com>
2025-04-30 21:51:00 +0200

    tabs

--- core.lua
+++ core.lua
@@ -1,4 +1,4 @@
-﻿local macro = CreateFrame('button', nil, nil, 'SecureActionButtonTemplate')macro:SetAttribute('type', 'macro')
+local macro = CreateFrame('button', nil, nil, 'SecureActionButtonTemplate')macro:SetAttribute('type', 'macro')
 macro:SetScript('OnLeave', function(self)
     self:Hide()
     GameTooltip_Hide()
@@ -19,14 +19,97 @@ hooksecurefunc('PlayerTalentFrameTalent_OnEnter', function(self)
 end)
 
 local function m()
-    if IsLoggedIn() and select(2,UnitClass("player"))=="WARRIOR" then
-    SetMacroSpell("Tier1Macro", GetSpellInfo("Неудержимость") or GetSpellInfo("Удвоенное время") or GetSpellInfo("Вестник войны"))
-    SetMacroSpell("Tier2Macro", GetSpellInfo("Безудержное восстановление") or GetSpellInfo("Верная победа") or GetSpellInfo("Второе дыхание") or GetSpellInfo("Победный раж"))
-    SetMacroSpell("Tier3Macro", GetSpellInfo("Ошеломляющий крик") or GetSpellInfo("Пронзительный вой") or GetSpellInfo("Разрушительный крик"))
-    SetMacroSpell("Tier4Macro", GetSpellInfo("Вихрь клинков") or GetSpellInfo("Рев дракона") or GetSpellInfo("Ударная волна"))
-    SetMacroSpell("Tier5Macro", GetSpellInfo("Массовое отражение заклинания") or GetSpellInfo("Охрана") or GetSpellInfo("Бдительность"))
-    SetMacroSpell("Tier6Macro", GetSpellInfo("Аватара") or GetSpellInfo("Кровавая баня") or GetSpellInfo("Удар громовержца"))
-    else return end
+    if IsLoggedIn() and select(2, UnitClass("player")) == "WARRIOR" then
+        -- Получаем индексы макросов
+        local tier1Macro = GetMacroIndexByName("Tier1Macro")
+        local tier2Macro = GetMacroIndexByName("Tier2Macro")
+        local tier3Macro = GetMacroIndexByName("Tier3Macro")
+        local tier4Macro = GetMacroIndexByName("Tier4Macro")
+        local tier5Macro = GetMacroIndexByName("Tier5Macro")
+        local tier6Macro = GetMacroIndexByName("Tier6Macro")
+
+        -- Функция для получения первого доступного заклинания
+        local function GetFirstAvailableSpell(...)
+            for i = 1, select("#", ...) do
+                local spell = select(i, ...)
+                local spellID = GetSpellInfo(spell)
+                if spellID then 
+                    return spellID 
+                end
+            end
+            return nil
+        end
+
+        -- Устанавливаем заклинания для каждого тира
+        if tier1Macro > 0 then 
+            local spell = GetFirstAvailableSpell(
+                "Неудержимость",
+                "Удвоенное время",
+                "Вестник войны"
+            )
+            if spell then 
+                SetMacroSpell(tier1Macro, spell) 
+            end
+        end
+
+        if tier2Macro > 0 then 
+            local spell = GetFirstAvailableSpell(
+                "Безудержное восстановление",
+                "Верная победа",
+                "Второе дыхание",
+                "Победный раж"
+            )
+            if spell then 
+                SetMacroSpell(tier2Macro, spell) 
+            end
+        end
+
+        if tier3Macro > 0 then 
+            local spell = GetFirstAvailableSpell(
+                "Ошеломляющий крик",
+                "Пронзительный вой",
+                "Разрушительный крик"
+            )
+            if spell then 
+                SetMacroSpell(tier3Macro, spell) 
+            end
+        end
+
+        if tier4Macro > 0 then 
+            local spell = GetFirstAvailableSpell(
+                "Вихрь клинков",
+                "Рев дракона",
+                "Ударная волна"
+            )
+            if spell then 
+                SetMacroSpell(tier4Macro, spell) 
+            end
+        end
+
+        if tier5Macro > 0 then 
+            local spell = GetFirstAvailableSpell(
+                "Массовое отражение заклинания",
+                "Охрана",
+                "Бдительность"
+            )
+            if spell then 
+                SetMacroSpell(tier5Macro, spell) 
+            end
+        end
+
+        if tier6Macro > 0 then 
+            local spell = GetFirstAvailableSpell(
+                "Аватара",
+                "Кровавая баня",
+                "Удар громовержца"
+            )
+            if spell then 
+                SetMacroSpell(tier6Macro, spell) 
+            end
+        end
+    else 
+        return 
+    end
 end
 local f = a or CreateFrame("Frame", "a")
 f:SetScript("OnEvent", m)
